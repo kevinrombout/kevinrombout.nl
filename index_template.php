a8174c6d2b8c80a7536748461fb50fb6e3985540
kevinrombout <kevin.rombout@atabix.nl>
2015-12-13 03:01:08 +0100

    add index

---
+++ index_template.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set("Europe/Amsterdam");
+
+/*****************************************/
+/*           BOOTSTRAP SYMFONY           */
+/*****************************************/
+
+    $nameSpace = 'Kevin';
+    $baseDir = __DIR__;
+    require_once $baseDir.'/vendor/autoload.php';
+
+
